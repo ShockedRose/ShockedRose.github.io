c19e599a8c4783af70daa8651428aed26751edf2
shockedRose <miguelj0916@gmail.com>
2026-04-13 19:58:32 -0500

    add sitemap, robots.txt, deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   site: "https://shockedrose.github.io",
 })
